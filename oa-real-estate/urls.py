dac4f1979f8c2c2ea5ee496f40a18289a81fb8d9
Rovshen Berdimyradov <rovshen.berdimyradov@emergn.com>
2024-05-02 14:47:13 +0400

    Added gitignore

--- oa-real-estate/urls.py
+++ oa-real-estate/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for OjarAziaHouse project.
+URL configuration for oa-real-estate project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
--- oa-real-estate/wsgi.py
+++ oa-real-estate/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for OjarAziaHouse project.
+WSGI config for oa-real-estate project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OjarAziaHouse.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oa-real-estate.settings')
 
 application = get_wsgi_application()
